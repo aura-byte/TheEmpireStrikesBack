dd791b4086d34dc0902df17a134596126b0f3b1c
aura-byte <81089587+aura-byte@users.noreply.github.com>
2022-07-27 16:47:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,9 +18,35 @@ def do_turn(pw):
     priority.sort(key=lambda y: y[0], reverse=True)
 
     for target in priority:
+        required_fleet_size = calculate_fleet_size(target)
 
-        required_fleet_size = target[2].num_ships() + (target[3] // 1.05) * target[2].growth_rate()
+        # attacking_fleets = []
+        counter_priority = []
+        if len(pw.enemy_fleets()) > 0:
+            for e_fleet in pw.enemy_fleets():
+                if e_fleet.destination_planet() == target[1]:
+                    # attacking_fleets.append(e_fleet)
+                    for mplanet in pw.my_planets():
+                        distance = pw.distance(source_planet=mplanet, destination_planet=e_fleet.source_planet())
+                        counter_priority.append([0, mplanet, e_fleet.source_planet(), distance])
+                    break
+
+        if len(counter_priority) > 0:
+            counter_priority.sort(key=lambda y: y[1], reverse=True)
+        for counter_target in counter_priority:
+            required_fleet_size = calculate_fleet_size(counter_target)
+            if counter_target[1].num_ships() > required_fleet_size * 1.5:
+                pw.issue_order(source_planet=counter_target[1], destination_planet=counter_target[2], num_ships=required_fleet_size)
+                return
 
         if target[1].num_ships() > required_fleet_size * 1.5:
             pw.issue_order(source_planet=target[1], destination_planet=target[2], num_ships=required_fleet_size)
             return
+
+
+def calculate_fleet_size(target):
+    if target[2].owner() == 2:
+        required_fleet_size = target[2].num_ships() + (target[3] // 1.05) * target[2].growth_rate()
+    else:
+        required_fleet_size = target[2].num_ships() * 1.1
+    return required_fleet_size
